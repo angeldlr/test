0b37c74c4f566c27063327e9a42c3e13431201be
Angel De La Rosa Alpizar <angeldlr@localhost.localdomain>
2018-07-28 06:31:41 -0500

    Se agrego el servicio de admi del cliente

---
+++ src/main/java/org/escom/jaxws/clientes/Clientes.java
@@ -0,0 +1,20 @@
+package org.escom.jaxws.clientes;
+
+import java.util.List;
+
+import javax.jws.WebService;
+
+@WebService
+public interface Clientes {
+	
+	public Cliente agregarCliente(Cliente cliente);
+	
+	public List<Cliente> obtenerClientes();
+	
+	public Cliente eliminarCliente(int id);
+	
+	public Cliente actualizarCliente(int idCliente,Cliente cliente);
+	
+	public Cliente obtenerClienteId(int id);
+	
+}
